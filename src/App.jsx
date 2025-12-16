74aedd4af1c04d960bdaaf171915708d8f1bc201
srivarshinidittakavi <varshinidittakavi04@gmail.com>
2025-12-16 22:39:58 +0530

    Added Calculator component with Add, Subtract, Multiply functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Counter from "./components/Counter";
+import Calculator from "./components/Calculator";
 
 function App() {
   return (
     <div>
       <h2>React Counter Example</h2>
       <Counter />
+      <Calculator/>
     </div>
   );
 }
